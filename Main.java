17a0e3837c11a12b38b80e555a1d67f4aee1750f
TugbaNurAlparslan <nurtugba2110@gmail.com>
2022-12-05 12:53:01 +0300

    added command line in mainclass

--- Main.java
+++ Main.java
@@ -4,8 +4,11 @@ import studentDbApp.Student;
 
 public class Main{
     public static void main(String[] args) {
+
+        //Student Database Project
         Student student1 = new Student();
         
+        //Email Administration project 
         Email email1 = new Email("Tugba", "Alparslan");
         email1.setAlternateEmail("java.@gmail.com");
         System.out.println(email1.getAlternateEmail());
